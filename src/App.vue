4a42079e28be15deaeebdc0c8962bf8079fcedf9
원치현 <heungeob1003@gmail.com>
2021-10-31 20:51:58 +0900

    Change background image's X coordinate every 10s by using interval function

--- src/App.vue
+++ src/App.vue
@@ -21,32 +21,38 @@
     바위:'0',
     가위:'-142px',
     보:'-284px'
-  }
+  };
+  let interval = null;
   export default {
     name: 'App',
     components: {
     },
     data(){
       return{
-        imageCoord: rspCoords.바위,
+        imgCoord: rspCoords.바위,
         result:'',
         score:0,
       }
     },
     computed:{
       computedStyleObject(){
-        return {background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imageCoord} 0`}
+        return {background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`}
       }
     },
-    created(){
-      console.log('created')
-    },
     mounted(){
-      console.log('mounted')
+      interval = setInterval(() => {
+        if(this.imgCoord === rspCoords.바위) {
+          this.imgCoord = rspCoords.가위;
+        } else if(this.imgCoord === rspCoords.가위) {
+          this.imgCoord = rspCoords.보;
+        } else if(this.imgCoord === rspCoords.보){
+          this.imgCoord = rspCoords.바위;
+        }
+      }, 100);  
     },
-    updated(){
-      console.log('updated')
-    },
+    beforeUnMount(){
+      clearInterval(interval);  
+    }
   }
 
 </script>
