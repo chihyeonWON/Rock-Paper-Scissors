26f12d640af088a133b307058b6868f7c90d7f9c
원치현 <heungeob1003@gmail.com>
2021-10-31 20:01:46 +0900

    Add rock,scissors,paper button, result and score

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-
+  <div id="computer">
+    <div>
+      <button>바위</button>
+      <button>가위</button>
+      <button>보</button>
+    </div>
+    <div>
+      {{ result }}
+    </div>
+    <div>
+      {{ score }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
   components: {
+  },
+  data(){
+    return{
+      result:'',
+      score:0,
+    }
   }
 }
 </script>
