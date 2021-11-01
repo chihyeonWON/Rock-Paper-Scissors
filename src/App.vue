9a2bd936428330566a6475f6e58b4accb4184f76
원치현 <heungeob1003@gmail.com>
2021-11-01 15:01:38 +0900

    Complete making rock paper scissors game

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,87 @@
 <template>
-<div>
-  <div id="computer" :style="computedStyleObject">
+  <div>
+    <div id="computer" :style="computedStyleObject"></div>
+    <div>
+      <button @click="onClickButton('바위')">바위</button>
+      <button @click="onClickButton('가위')">가위</button>
+      <button @click="onClickButton('보')">보</button>
+    </div>
+    <div>{{result}}</div>
+    <div>현재 {{score}}점</div>
   </div>
-    <div>
-      <button @onclick = "onClickButton('바위')">바위</button>
-      <button @onclick = "onClickButton('가위')">가위</button>
-      <button @onclick = "onClickButton('보')">보</button>
-    </div>
-    <div>
-      {{ result }}
-    </div>
-    <div>
-      현재 {{ score }}점
-    </div>
-</div>
 </template>
 
 <script>
   const rspCoords = {
-    바위:'0',
-    가위:'-142px',
-    보:'-284px'
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
   };
-  
   const scores = {
-    가위:1,
-    바위:0,
-    보:-1,
-  }
-
+    가위: 1,
+    바위: 0,
+    보: -1,
+  };
   const computerChoice = (imgCoord) => {
-    return Object.entries(rspCoords).find(function(v){
+    return Object.entries(rspCoords).find(function (v) {
       return v[1] === imgCoord;
     })[0];
-  }
-  
+  };
   let interval = null;
   export default {
-    name: 'App',
-    components: {
-    },
-    data(){
-      return{
+    data() {
+      return {
         imgCoord: rspCoords.바위,
-        result:'',
-        score:0,
+        result: '',
+        score: 0,
+      };
+    },
+    computed: {
+      computedStyleObject() {
+        return {
+          background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`,
+        };
       }
     },
-    computed:{
-      computedStyleObject(){
-        return {background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`}
-      }
-    },
-    mounted(){
-      interval = setInterval(() => {
-        if(this.imgCoord === rspCoords.바위) {
-          this.imgCoord = rspCoords.가위;
-        } else if(this.imgCoord === rspCoords.가위) {
-          this.imgCoord = rspCoords.보;
-        } else if(this.imgCoord === rspCoords.보){
-          this.imgCoord = rspCoords.바위;
+    methods: {
+      changeHand() {
+        interval = setInterval(() => {
+          if (this.imgCoord === rspCoords.바위) {
+            this.imgCoord = rspCoords.가위;
+          } else if (this.imgCoord === rspCoords.가위) {
+            this.imgCoord = rspCoords.보;
+          } else if (this.imgCoord === rspCoords.보) {
+            this.imgCoord = rspCoords.바위;
+          }
+        }, 100);
+      },
+      onClickButton(choice) {
+        clearInterval(interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(this.imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+          this.result = '비겼습니다.';
+        } else if ([-1, 2].includes(diff)) {
+          this.result = '이겼습니다.';
+          this.score += 1;
+        } else {
+          this.result = '졌습니다.';
+          this.score -= 1;
         }
-      }, 100);  
+        setTimeout(() => {
+          this.changeHand();
+        }, 1000);
+      },
     },
-    beforeUnMount(){
-      clearInterval(interval);  
+    mounted() {
+      console.log('mounted');
+      this.changeHand();
+    },
+    beforeUnmount() {
+      clearInterval(interval);
     }
-  }
-
+}
 </script>
 
 <style>
